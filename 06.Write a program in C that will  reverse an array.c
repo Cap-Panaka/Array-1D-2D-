3ec35f8f461a37f0a906f753de9c6bde42030853
Cap-Panaka <nerdcrab1122@gmail.com>
2025-08-16 00:11:49 +0600

    Update 06.Write a program in C that will  reverse an array.c

--- 06.Write a program in C that will  reverse an array.c
+++ 06.Write a program in C that will  reverse an array.c
@@ -25,3 +25,33 @@ int main()
         printf("%d ",ar[i]);
     }
 }
+
+/* Swap Method: (Mustafina ma'am)
+#include <stdio.h>
+int main() 
+{
+    int n,i;
+    printf("Enter the number of elements for your array: ");
+    scanf("%d",&n);
+    int ar[100];
+    printf("Enter the array elements: \n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(i=0;i<n/2;i++){
+        int temp=ar[i];
+        ar[i]=ar[n-1-i];
+        ar[n-1-i]=temp;
+    }
+    printf("Your array after reversing: \n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",ar[i]);
+    }
+    
+}*/
+
+
+
+
